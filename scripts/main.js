1db4ff7c6a4dbbcecae10c2b78f24e1c643cb0c4
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2020-12-15 18:18:49 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 Vars.enableConsole = true;
 
+const tantros = new Planet("tantros", Planets.sun, 2, 0.8)
+tantros.generator = new TantrosPlanetGenerator();
+tantros.meshLoader = () => new HexMesh(this, 4);
+tantros.atmosphereColor = Color.valueOf("3db899");
+tantros.startSector = 10;
+tantros.atmosphereRadIn = -0.01;
+tantros.atmosphereRadOut = 0.3;
+
 Vars.content.blocks().each(e => {e.health = 40});
 Vars.content.blocks().each(e => {
     if(e.size === 4 ) e.size = 2;
